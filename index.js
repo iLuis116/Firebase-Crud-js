d82fbf81aa5dee2da67d3a956747f4a7a5764ed3
Jose Luis Nava Flores <luisflores2103@gmail.com>
2022-11-27 14:48:50 -0600

    Setup del sistema

---
+++ index.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyACsjyVPBHGSRwGbPR7v_A3ZE6JNRVBzNI",
+    authDomain: "fir-crud-33dd6.firebaseapp.com",
+    projectId: "fir-crud-33dd6",
+    storageBucket: "fir-crud-33dd6.appspot.com",
+    messagingSenderId: "839628503064",
+    appId: "1:839628503064:web:65c937036b92b25dd6eb6d"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
